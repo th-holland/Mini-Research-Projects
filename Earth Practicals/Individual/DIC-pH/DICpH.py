824858f8feb3081d21fe426becea2483cce5af9b
th-holland <101332229+th-holland@users.noreply.github.com>
2022-11-22 14:23:18 +0000

    added models and plots

--- Earth Practicals/Individual/DIC-pH/DICpH.py
+++ Earth Practicals/Individual/DIC-pH/DICpH.py
@@ -1,28 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# import package for Ka values 
 ##### ALL VALUES:
 ## TEMP deg C
+# Ka x K-a = Kw = 10**-14
 
-def deltaCO2(_deltaT, _pCO2):
-    return 0.0423 * _deltaT * _pCO2
 
-def dCO2dt(_CO2, _k1, _H, _HCO3):
-    return -_k1 * _CO2 + 1/_k1 * _H * _HCO3
+### generate DIC species pH model
 
-def dHdt(_CO2, _k1, _H, _HCO3, _k2, _CO3):
-    return _k1 * _CO2 - 1/_k1 * _H * _HCO3 + _k2 * _HCO3 - 1/_k2 * _H * _CO3
+## define finite state machine matrix for set of equilibrium
 
-def dHCO3dt(_CO2, _k1, _H, _HCO3, _k2, _CO3):
-    return _k1 * _CO2 - 1/_k1 * _H * _HCO3 - _k2 * _HCO3 + 1/_k2 * _H * _CO3
+# CO2 + H2O ⇌ H+ + HCO−3 (forward = k1, reverse = 10**14/k1)
+# HCO−3 ⇌ H+ + CO2−3 (forward = k2, reverse = 10**14/k2)
+# k1 = 10**-6.3
+# k2 = 10**-10.3
+ 
+def dCO2dt(kw, k1, k2, CO2, H, HCO3):
+    return -k1 * CO2 + kw/k1 * H * HCO3
 
-def dCO3dt(_k2, _H, _CO3):
-    return _k2 * _H - 1/_k2 * _H * _CO3
+def dHdt(kw, k1, k2, CO2, H, HCO3, CO3):
+    return k1 * CO2 - kw/k1 * H * HCO3 +k2 * HCO3 - kw/k2 * H * CO3
+
+def dHCO3dt(kw, k1, k2, CO2, H, HCO3, CO3):
+    return k1 * CO2 - kw/k1 * H * HCO3 - k2 * HCO3 + kw/k2 * H * CO3
+
+def dCO3dt(kw, k2, H, HCO3, CO3):
+    return k2 * HCO3 - kw/k2 * H * CO3
 
 
 def model(_params):
     # initial conditions
+    kw = _params['kw']
     CO2 = _params['pCO2']
     H = _params['H']
     HCO3 = _params['HCO3']
@@ -31,9 +39,6 @@ def model(_params):
     k2 = _params['k2']
     dt = _params['dt']
     time = _params['time']
-    years = _params['years']
-    pCO2 = _params['pCO2']
-    deltaT = _params['dt']
     # generate model
     CO2_model = np.zeros(len(time))
     H_model = np.zeros(len(time))
@@ -44,8 +49,9 @@ def model(_params):
         H_model[i] = H
         HCO3_model[i] = HCO3
         CO3_model[i] = CO3
-        CO2 += dCO2dt(CO2, k1, H, HCO3) * dt
-        H += dHdt(CO2, k1, H, HCO3, k2, CO3) * dt
-        HCO3 += dHCO3dt(CO2, k1, H, HCO3, k2, CO3) * dt
-        CO3 += dCO3dt(k2, H, CO3) * dt
+        CO2 += dCO2dt(kw, k1, k2, CO2, H, HCO3) * dt
+        H += dHdt(kw, k1, k2, CO2, H, HCO3, CO3) * dt
+        HCO3 += dHCO3dt(kw, k1, k2, CO2, H, HCO3, CO3) * dt
+        CO3 += dCO3dt(kw, k2, H, HCO3, CO3) * dt
+
     return CO2_model, H_model, HCO3_model, CO3_model
--- Earth Practicals/Individual/DIC-pH/run.py
+++ Earth Practicals/Individual/DIC-pH/run.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from DICpH import model
 from math import exp
+import csv
 
 # initial conditions
 
@@ -9,44 +10,57 @@ from math import exp
 
 params = {
     'pCO2': 0.041472,
-    'H': 10**-7.0,
-    'HCO3': 0,
-    'CO3': 0,
+    'H': 10**-8.1,
+    'HCO3': 0.02,
+    'CO3': 0.02,
+    'kw': 10**-14,
     'k1': 10**-6.3,
     'k2': 10**-10.3,
-    'dt': 0.1,
-    'years': 100,
+    'dt': 0.01,
+    'years': 10000,
     'time': 0
 }
 
 # calculate time array
 params['time'] = np.arange(0, params['years'], params['dt'])
 
-print(params['time'])
-
 # run model
 
 CO2_model, H_model, HCO3_model, CO3_model = model(params)
 
-# print model in table of every 10 years
+# print model in table of every 10 years into csv file
 
-for i in range(0, len(params['time']), 100):
-    print('Year: ', params['time'][i], 'CO2: ', CO2_model[i], 'H: ', H_model[i], 'HCO3: ', HCO3_model[i], 'CO3: ', CO3_model[i])
+with open('Earth Practicals/Individual/DIC-pH/output.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['time', 'CO2', 'H', 'HCO3', 'CO3'])
+    for i in range(len(params['time'])):
+        writer.writerow([params['time'][i], CO2_model[i], H_model[i], HCO3_model[i], CO3_model[i]])
 
-## convert all to log scale
+# plot pH
 
-H_model = np.log10(H_model)
-HCO3_model = np.log10(HCO3_model)
-CO3_model = np.log10(CO3_model)
-C02_model = np.log10(CO2_model)
+pH_model = -np.log10(H_model)
 
-# plot model
-
-plt.plot(params['time'], H_model, label='H')
-plt.plot(params['time'], HCO3_model, label='HCO3')
-plt.plot(params['time'], CO3_model, label='CO3')
-plt.plot(params['time'], CO2_model, label='CO2')
-plt.xlim(0, 100)
+plt.plot(params['time'], pH_model)
 plt.legend()
+plt.xlabel('time (years)')
+plt.ylabel('pH')
+plt.show()
+
+# normalise CO2, HCO3 and CO3 to their respective starts
+
+CO2_model_normalised = np.divide(CO2_model, CO2_model[0])
+HCO3_model_normalised = np.divide(HCO3_model, HCO3_model[0])
+CO3_model_normalised = np.divide(CO3_model, CO3_model[0])
+
+CO2_model_normalised = np.exp(np.exp(CO2_model_normalised))
+HCO3_model_normalised = np.exp(np.exp(HCO3_model_normalised))
+CO3_model_normalised = np.exp(np.exp(CO3_model_normalised))
+
+plt.plot(params['time'], CO2_model_normalised)
+plt.plot(params['time'], HCO3_model_normalised)
+plt.plot(params['time'], CO3_model_normalised)
+plt.legend()
+plt.xlabel('time (years)')
+plt.ylabel('log_[100]DIC (ppm)')
 plt.show()
 
